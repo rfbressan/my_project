eb3e30a01476a244d45db2c40658b7fbec19563e
Rafael Bressan <rfbressan@gmail.com>
2022-02-06 10:16:51 -0300

    fix: correct table position relative to footnote

--- R/functions_tables.R
+++ R/functions_tables.R
@@ -1,7 +1,11 @@
 # functions_tables.R
 
 hello_tbl <- function(hello_dt) {
-  kbl(hello_dt, format = "latex", booktabs = TRUE) |> 
+  kbl(hello_dt, format = "latex", booktabs = TRUE,
+      caption = "Hello world number of characters",
+      label = "hello",
+      col.names = c("String", "# characters")) |> 
+    kable_classic(latex_options = "HOLD_position") |> 
     save_kable("Article/Tables/hello.tex")
   
   return("Article/Tables/hello.tex")
